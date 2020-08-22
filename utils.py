6d6e5983aabe94eaa7be72684ee81ef558439a8c
Stephen Yang <stepheny755@gmail.com>
2020-08-22 10:35:05 -0700

    added extension changing code

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
-import librosa
+import librosa, os
 
 class Utils():
 
-    def __init__():
+    def __init__(self):
+        pass
 
     def load_audio(self,file_path,target_sr):
         signal, sample_rate = librosa.load(file_path,sr=target_sr,mono=True)
         return signal, sample_rate
+
+    def change_ext(self,filename,out):
+        return os.path.splitext(filename)[0]+out
