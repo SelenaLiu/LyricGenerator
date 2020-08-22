bc77f4e7df0c3a60b63a7c757880daae5c83a980
Stephen Yang <stepheny755@gmail.com>
2020-08-22 09:12:29 -0700

    added function prototypes for preprocessing audio data

---
+++ preprocess.py
@@ -0,0 +1,16 @@
+from utils import Utils
+
+class Preprocess():
+
+    def __init__():
+        self.ut = Utils()
+
+    def get_max_length():
+        #needs code
+        #gets longest song from entire array
+        pass
+
+    def equalize_length():
+        #needs code
+        #pads all songs at end to be same size as max length
+        pass
