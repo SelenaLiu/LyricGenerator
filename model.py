b35e92602eed58b8347c5c43791d217a1d8c443e
Stephen Yang <stepheny755@gmail.com>
2020-08-22 09:50:41 -0700

    added model code

---
+++ model.py
@@ -0,0 +1,19 @@
+from keras.layers import Bidirectional, Concatenate, Permute, Dot, Input, LSTM, Multiply
+from keras.layers import RepeatVector, Dense, Activation, Lambda
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+from keras.models import load_model, Model
+
+import keras.backend as K
+import numpy as np
+import tensorflow as tf
+import os, librosa
+
+from utils import Utils
+
+#Define constants
+use_gpu = (len(tf.config.experimental.list_physical_devices('GPU'))>0)?True:False
+target_sr = 44100
+
+audio_list = []
+label_list = []
