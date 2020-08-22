2102c7fde8ee807dae3a52b2d9f70d4af974f16e
Isha Sharma <isha.sha14@gmail.com>
2020-08-22 10:41:34 -0700

    Finished process_data function

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,22 @@
 from utils import Utils
+import numpy as np
 
 class Preprocess():
 
     def __init__():
         self.ut = Utils()
 
-    def get_max_length():
-        #needs code
+    def process_data(self, list_songs: List):
         #gets longest song from entire array
-        pass
+        list_songs.sort(key=len)
+        max_length = list_songs[-1].shape[0]
+        for element in list_songs:
+            element_len = element.shape[0]
+            N = max_length - element_len
+            np.pad(element, (0, N), 'constant')
 
-    def equalize_length():
-        #needs code
-        #pads all songs at end to be same size as max length
-        pass
+
+    # def equalize_length():
+    #     #needs code
+    #     #pads all songs at end to be same size as max length
+    #     pass
