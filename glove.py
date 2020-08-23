13d67ed3be834f1423f10261b6a391f1e7879ad5
Isha Sharma <isha.sha14@gmail.com>
2020-08-22 19:22:04 -0700

    Create glove.py with code for word_embeddings

---
+++ glove.py
@@ -0,0 +1,41 @@
+import numpy as np
+from scipy import spatial
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+
+def create_embeddings_dict():
+    embeddings_dict = {}
+    with open("glove.6B.50d.txt", 'r', encoding="utf-8") as f:
+        # replace glove.6B...txt with our text file name
+        for line in f:
+            values = line.split()
+            word = values[0]
+            vector = np.asarray(values[1:], "float32")
+            embeddings_dict[word] = vector
+
+
+def find_closest_embeddings(embedding):
+    return sorted(embeddings_dict.keys(),
+                  key=lambda word: spatial.distance.euclidean(
+                      embeddings_dict[word], embedding))
+
+
+# we would replace king
+print(find_closest_embeddings(embeddings_dict["king"])[1:6])
+
+# we would also replace twig, branch, and hand
+print(find_closest_embeddings(
+    embeddings_dict["twig"] - embeddings_dict["branch"] +
+    embeddings_dict["hand"])[:5])
+
+# tsne = TSNE(n_components=2, random_state=0)
+# words =  list(embeddings_dict.keys())
+# vectors = [embeddings_dict[word] for word in words]
+# Y = tsne.fit_transform(vectors[:1000])
+#
+# plt.scatter(Y[:, 0], Y[:, 1])
+#
+# for label, x, y in zip(words, Y[:, 0], Y[:, 1]):
+#     plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords="offset points")
+# plt.show()
