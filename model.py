28581612d940429046caa4bb1989de5cef318f9a
Isha Sharma <isha.sha14@gmail.com>
2020-08-22 17:37:18 -0700

    Added CONV1D layers

--- model.py
+++ model.py
@@ -42,19 +42,42 @@ num_hidden = 50
 num_layers = 1
 batch_size = 128
 
-train_seq_len = [(14553000, 128)]
+# train_audio_tensor = torch.from_numpy(train_audio)
+# train_txt_tensor = torch.from_numpy(train_txt)
+#
+# train_dataset = TensorDataset(train_audio_tensor, train_txt_tensor)
+# train_dataloader = DataLoader(train_dataset)
+#
+# val_audio_tensor = torch.from_numpy(val_audio)
+# val_txt_tensor = torch.from_numpy(val_txt)
+#
+# val_dataset = TensorDataset(val_audio_tensor, val_txt_tensor)
+# val_dataloader = DataLoader(val_dataset)
 
-train_audio_tensor = torch.from_numpy(train_audio)
-train_txt_tensor = torch.from_numpy(train_txt)
-
-train_dataset = TensorDataset(train_audio_tensor, train_txt_tensor)
-train_dataloader = DataLoader(train_dataset)
-
-val_audio_tensor = torch.from_numpy(val_audio)
-val_txt_tensor = torch.from_numpy(val_txt)
-
-val_dataset = TensorDataset(val_audio_tensor, val_txt_tensor)
-val_dataloader = DataLoader(val_dataset)
-
+inputs = Input(shape=(14553000, 128))
+x = BatchNormalization(axis=-1, momentum=0.99, epsilon=1e-3, center=True, scale=True)(inputs)
+#First Conv1D layer
+x = Conv1D(8,13, padding='valid', activation='relu', strides=1)(x)
+x = MaxPooling1D(3)(x)
+x = Dropout(0.3)(x)
+#Second Conv1D layer
+x = Conv1D(16, 11, padding='valid', activation='relu', strides=1)(x)
+x = MaxPooling1D(3)(x)
+x = Dropout(0.3)(x)
+#Third Conv1D layer
+x = Conv1D(32, 9, padding='valid', activation='relu', strides=1)(x)
+x = MaxPooling1D(3)(x)
+x = Dropout(0.3)(x)
+x = BatchNormalization(axis=-1, momentum=0.99, epsilon=1e-3, center=True, scale=True)(x)
+x = Bidirectional(CuDNNGRU(128, return_sequences=True), merge_mode='sum')(x)
+x = Bidirectional(CuDNNGRU(128, return_sequences=True), merge_mode='sum')(x)
+x = Bidirectional(CuDNNGRU(128, return_sequences=False), merge_mode='sum')(x)
+x = BatchNormalization(axis=-1, momentum=0.99, epsilon=1e-3, center=True, scale=True)(x)
+#Flatten layer
+# x = Flatten()(x)
+#Dense Layer 1
+x = Dense(256, activation='relu')(x)
+outputs = Dense(len(labels), activation="softmax")(x)
 model = Model(inputs, outputs)
+model.summary()
 
