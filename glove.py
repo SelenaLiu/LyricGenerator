c2249f1860eeb12edf631c0d1ec01da90a20f57b
Isha Sharma <isha.sha14@gmail.com>
2020-08-22 21:01:06 -0700

    Added keras embedding code and removed glove code

--- glove.py
+++ glove.py
@@ -1,41 +1,42 @@
-import numpy as np
-from scipy import spatial
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-
-
-def create_embeddings_dict():
-    embeddings_dict = {}
-    with open("glove.6B.50d.txt", 'r', encoding="utf-8") as f:
-        # replace glove.6B...txt with our text file name
-        for line in f:
-            values = line.split()
-            word = values[0]
-            vector = np.asarray(values[1:], "float32")
-            embeddings_dict[word] = vector
-
-
-def find_closest_embeddings(embedding):
-    return sorted(embeddings_dict.keys(),
-                  key=lambda word: spatial.distance.euclidean(
-                      embeddings_dict[word], embedding))
-
-
-# we would replace king
-print(find_closest_embeddings(embeddings_dict["king"])[1:6])
-
-# we would also replace twig, branch, and hand
-print(find_closest_embeddings(
-    embeddings_dict["twig"] - embeddings_dict["branch"] +
-    embeddings_dict["hand"])[:5])
-
-# tsne = TSNE(n_components=2, random_state=0)
-# words =  list(embeddings_dict.keys())
-# vectors = [embeddings_dict[word] for word in words]
-# Y = tsne.fit_transform(vectors[:1000])
-#
-# plt.scatter(Y[:, 0], Y[:, 1])
-#
-# for label, x, y in zip(words, Y[:, 0], Y[:, 1]):
-#     plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords="offset points")
-# plt.show()
+from numpy import array
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.embeddings import Embedding
+# define documents
+docs = ['Well done!',
+        'Good work',
+        'Great effort',
+        'nice work',
+        'Excellent!',
+        'Weak',
+        'Poor effort!',
+        'not good',
+        'poor work',
+        'Could have done better.']
+# define class labels
+labels = array([1,1,1,1,1,0,0,0,0,0])
+# integer encode the documents
+vocab_size = 50
+encoded_docs = [one_hot(d, vocab_size) for d in docs]
+print(encoded_docs)
+# pad documents to a max length of 4 words
+max_length = 4
+padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+print(padded_docs)
+# define the model
+model = Sequential()
+model.add(Embedding(vocab_size, 8, input_length=max_length))
+model.add(Flatten())
+model.add(Dense(1, activation='sigmoid'))
+# compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# summarize the model
+print(model.summary())
+# fit the model
+model.fit(padded_docs, labels, epochs=50, verbose=0)
+# evaluate the model
+loss, accuracy = model.evaluate(padded_docs, labels, verbose=0)
+print('Accuracy: %f' % (accuracy*100))
