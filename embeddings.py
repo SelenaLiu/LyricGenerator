8c0eb200b8a27962b9f7a9aa07eb892529cb61fa
jhimanish <54194957+jhimanish@users.noreply.github.com>
2020-08-23 02:05:29 -0400

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy import array
 from keras.preprocessing.text import one_hot
 from keras.preprocessing.sequence import pad_sequences
@@ -5,38 +6,38 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers.embeddings import Embedding
+
 # define documents
-docs = ['Well done!',
-        'Good work',
-        'Great effort',
-        'nice work',
-        'Excellent!',
-        'Weak',
-        'Poor effort!',
-        'not good',
-        'poor work',
-        'Could have done better.']
+docs = np.loadtxt(drive_location + 'all_words.txt', dtype=str, delimiter=",")
+
 # define class labels
-labels = array([1,1,1,1,1,0,0,0,0,0])
+labels = array([1]*242891)
+
 # integer encode the documents
-vocab_size = 50
+vocab_size = 245000  # number of words plus some extra
 encoded_docs = [one_hot(d, vocab_size) for d in docs]
 print(encoded_docs)
+
 # pad documents to a max length of 4 words
-max_length = 4
+max_length = 4 # change to 1 since we only have individual words
 padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
 print(padded_docs)
+
 # define the model
 model = Sequential()
 model.add(Embedding(vocab_size, 8, input_length=max_length))
 model.add(Flatten())
 model.add(Dense(1, activation='sigmoid'))
+
 # compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
 # summarize the model
 print(model.summary())
+
 # fit the model
-model.fit(padded_docs, labels, epochs=50, verbose=0)
+model.fit(padded_docs, labels, epochs=10, verbose=0)
+
 # evaluate the model
 loss, accuracy = model.evaluate(padded_docs, labels, verbose=0)
 print('Accuracy: %f' % (accuracy*100))
